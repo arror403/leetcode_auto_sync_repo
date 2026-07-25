dcbb494845a28c9ae064251f74be853aea86999f
arror403 <huang211395@gmail.com>
2026-07-25 09:21:36 +0800

    3859-MAXIMUM-PRODUCT-OF-TWO-DIGITS Stats: Time: 0 ms (100%), Space: 19.1 MB (88.69%) - LeetHub

--- 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.py
+++ 3859-maximum-product-of-two-digits/3859-maximum-product-of-two-digits.py
@@ -1,3 +1,4 @@
 class Solution:
     def maxProduct(self, n: int) -> int:
-        return max(int(a)*int(b) for a,b in combinations(str(n),2))
+        d=sorted(list(map(int,str(n))))
+        return d[-1]*d[-2]
