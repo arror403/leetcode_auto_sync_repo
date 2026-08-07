831e7d1c2e0982a85fb83851342aca1a61eefeb2
arror403 <huang211395@gmail.com>
2026-08-07 11:49:45 +0800

    1664-FIND-USERS-WITH-VALID-E-MAILS Stats: Time: 286 ms (82.97%), Space: 67.4 MB (61.2%) - LeetHub

--- 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
+++ 1664-find-users-with-valid-e-mails/1664-find-users-with-valid-e-mails.py
@@ -1,17 +1,5 @@
 import pandas as pd
 
 def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
-    res=[]
-    p=re.compile('^[a-zA-Z]+[\w\.\-]*(@leetcode\.com){1}$')
-
-    for _,r in users.iterrows():
-        m=p.match(r['mail'])
-        if m:
-            res.append([r['user_id'], r['name'], r['mail']])
-
-
-    return pd.DataFrame({
-        'user_id': [x[0] for x in res],
-        'name': [x[1] for x in res],
-        'mail': [x[2] for x in res]
-    })
+    mask = users['mail'].str.match(r'^[a-zA-Z]+[\w.\-]*@leetcode\.com$', na=False)
+    return users.loc[mask, users.columns].reset_index(drop=True)
